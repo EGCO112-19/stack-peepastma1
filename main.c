ff9869144ac5246767a44cd73bfa10ece8367c60
peepastma1 <natthabodi.boc@student.mahidol.edu>
2021-02-26 11:24:01 +0000

    mai wai raw

--- main.c
+++ main.c
@@ -5,25 +5,73 @@
 
 int main(int argc, char **argv){
   printf("Checking the parentheses in argv arguments\n");
-  int i,N,j;
-  
+  int i,N,j,check=0;
+  char out;
   Stack s;
-
-
+  s.size=0;
+  s.top=NULL;
  
   for(i=1;i<argc;i++){
    
      for(j=0;j<strlen(argv[i]);j++){
        /* Use stack to help with the parentheses*/
-
-
+      if(argv[i][j] == '{')
+       {
+         push(&s,argv[i][j]);
+         s.size +=1;
+       }
+      if(argv[i][j] == '[')
+       {
+         push(&s,argv[i][j]);
+         s.size +=1;
+       }
+      if(argv[i][j] == '}')
+        {
+          out = pop(&s);
+          s.size -= 1;
+          if(out != '{')
+          {
+             check = 1;
+             break;
+          }
+      }
+      if(argv[i][j] == ']')
+        {
+          out = pop(&s);
+          s.size -= 1;
+          if(out != '[')
+          {
+            check = 1;
+            break;
+          }
+      }
+      /*switch(argv[i][j])
+      {
+        case '{' :
+         push(&s,argv[i][j]);
+         s.size +=1;
+        case '[' :
+         push(&s,argv[i][j]);
+         s.size +=1;
+        case '}' :
+         if(s.top!=NULL){
+         out = pop(&s);
+         s.size -= 1;
+         if(out != '{'){check = 1;break;}}
+        case ']' :
+         if(s.top!=NULL){
+         out = pop(&s);
+         s.size -= 1;
+         if(out != '['){check = 1;break;}}
+      }*/
      }
-
-
+     if(s.size>0 || check == 1){
+       printf("The parentheses do not match for %s\n", argv[i]);
+       pop_all(&s);
+       check = 0;
+     }
+     else{
+       printf("The parentheses match successfully for %s\n",argv[i]);}
   }
-
-
-
-
    return 0;
 }
--- stack.h
+++ stack.h
@@ -8,7 +8,40 @@ typedef struct {
 }Stack;
 
 typedef Stack * StackPtr;
-void push(StackPtr s, char value);
-char pop(StackPtr s);
-void pop_all(StackPtr s);
+void push(StackPtr s, char value)
+{
+  Node *new_node=(Node *)malloc(sizeof(Node));
+  if(new_node!=NULL)
+  {
+    new_node->data=value;
+    new_node->nextPtr=s->top;
+    s->top=new_node;
+  }
+}
+char pop(StackPtr s)
+{
+  NodePtr t =s->top;
+  char value;
+  if(s->top != NULL)
+  {
+  value=t->data;
+  s->top=t->nextPtr;
+  free(t);
+  return value;
+  }
+}
+void pop_all(StackPtr s)
+{
+  NodePtr t =s->top;
+  char value;
+  while(s->top != NULL)
+  {
+    value=t->data;
+    s->top=t->nextPtr;
+    printf("Popping %c\n",value);
+    free(t);
+    t = s->top;
+    s->size = 0;
+  }
+}
 #endif
